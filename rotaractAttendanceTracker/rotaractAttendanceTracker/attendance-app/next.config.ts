68f20316f1bd42dda03f03ec61a10fa4cae280b8
Akash Pandey <pandeyakash457@gmail.com>
2025-06-24 02:37:51 +0530

    fix: configure export for railway

--- rotaractAttendanceTracker/rotaractAttendanceTracker/attendance-app/next.config.ts
+++ rotaractAttendanceTracker/rotaractAttendanceTracker/attendance-app/next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
