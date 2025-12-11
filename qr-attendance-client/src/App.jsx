da9081735b1ff3dd0bac8e3566b94feedbdcc579
ayush-021 <ayush@021.trade>
2025-12-12 00:22:49 +0530

    changes for rise

--- qr-attendance-client/src/App.jsx
+++ qr-attendance-client/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>NEXT The conclave</h1>
+      <h1>RISE 25-26</h1>
 
       <div className="card">
         <h2>Place the QR in front of the camera</h2>
